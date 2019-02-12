813e5e751ca7d94e6e04e49b1b9d89f879dbf993
James Carlson <jxxcarlson@mac.com>
2019-02-12 15:47:27 -0500

    record parser now working.

--- src/DataParser.elm
+++ src/DataParser.elm
@@ -1,8 +1,12 @@
-module DataParser exposing
-    ( Data
-    , Record
-    , string
-    )
+module DataParser
+    exposing
+        ( Data
+        , Record
+        , field
+        , nextString
+        , nextString2
+        , record
+        )
 
 import Parser exposing (..)
 import Parser.Extras exposing (many)
@@ -16,14 +20,56 @@ type alias Data =
     List Record
 
 
-string : Parser String
-string =
+record : Parser (List String)
+record =
+    loop [] recordGofer
+
+
+recordGofer : List String -> Parser (Step (List String) (List String))
+recordGofer revFields =
+    oneOf
+        [ succeed (\s -> Loop (s :: revFields))
+            |= field
+        , succeed ()
+            |> map (\_ -> Done (List.reverse revFields))
+        ]
+
+
+{-| A field as defined here begins with an alphanumeric
+character or '-' and is surrounded by spaces.
+-}
+field : Parser String
+field =
     (succeed String.slice
         |= getOffset
         |. chompWhile (\c -> c == ' ')
+        |. chompIf (\c -> Char.isAlphaNum c || c == '-')
         |. chompWhile (\c -> c /= ' ' && c /= '\n')
         |. chompWhile (\c -> c == ' ')
         |= getOffset
         |= getSource
     )
         |> map String.trim
+
+
+
+--
+-- EXPERIMENTS
+--
+
+
+{-| run (field |> andThen (\s -> nextString s)) "a b c"
+-}
+nextString : String -> Parser (List String)
+nextString s =
+    field |> map (\t -> [ s, t ])
+
+
+{-| run (field |> map (\s -> [s]) |> andThen (\s -> nextString2 s) |> andThen (\s -> nextString2 s)) "a b c"
+
+> Ok ["c","b","a"]
+
+-}
+nextString2 : List String -> Parser (List String)
+nextString2 stringList =
+    field |> map (\t -> t :: stringList)
