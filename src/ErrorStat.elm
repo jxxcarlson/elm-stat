8560e7d40ff2ffcb81aee42b9434d82403a34138
James Carlson <jxxcarlson@mac.com>
2019-02-18 01:19:58 +0100

    Aadd: ErrStat.errorBars

--- src/ErrorStat.elm
+++ src/ErrorStat.elm
@@ -1,7 +1,8 @@
-module ErrorStat exposing (ErrorDatum, get)
+module ErrorStat exposing (ErrorDatum, ErrorStats, get, mean, errorBars)
 
 import Types exposing (Point, Data)
 import Dict exposing (Dict)
+import Utility
 
 
 type alias ErrorDatum =
@@ -10,12 +11,80 @@ type alias ErrorDatum =
     }
 
 
-type alias MaybeErrorStats =
-    { mean : Maybe Float
+type alias ErrorStats =
+    { x : Float
+    , mean : Maybe Float
     , min : Maybe Float
     , max : Maybe Float
     , stdev : Maybe Float
     }
+
+
+type alias ErrorBar =
+    { x : Float
+    , top : Float
+    , mid : Float
+    , bottom : Float
+    }
+
+
+type alias ErrorData =
+    List ErrorDatum
+
+
+type alias ErrorDictionary =
+    Dict Float (List Float)
+
+
+errorBar : Float -> ErrorStats -> Maybe ErrorBar
+errorBar p es =
+    case ( es.mean, es.stdev ) of
+        ( Just m, Just s ) ->
+            Just
+                { x = es.x
+                , top = m + p * s
+                , mid = m
+                , bottom = m - p * s
+                }
+
+        ( _, _ ) ->
+            Nothing
+
+
+{-| Use to ompute error bars, as in this example:
+
+    > SampleData.eb2 |> Data.fromString 0 1 |> ErrorStat.get |> ErrorStat.errorBars 0.5
+      [{ bottom = 0.99667, mid = 1, top = 1.003333, x = 0 }
+      ,{ bottom = 1.98, mid = 2, top = 2.02, x = 1 }]
+
+-}
+errorBars : Float -> List ErrorStats -> List ErrorBar
+errorBars p ess =
+    List.map (errorBar p) ess
+        |> Utility.maybeValues
+
+
+{-| Use to ompute the y-centroids of the data, as in this example:
+
+> SampleData.eb2 |> Data.fromString 0 1 |> ErrorStat.get |> ErrorStat.mean
+> [{ x = 0, y = 1 },{ x = 1, y = 2 }]
+
+-}
+mean : List ErrorStats -> Data
+mean ess =
+    ess
+        |> List.map meanValue
+        |> Utility.maybeValues
+
+
+meanValue : ErrorStats -> Maybe Point
+meanValue es =
+    case es.mean of
+        Nothing ->
+            Nothing
+
+        Just y ->
+            Just { x = es.x, y = y }
 
 
 {-|
@@ -24,7 +93,7 @@ type alias MaybeErrorStats =
        [ { max = Just 1.1, mean = Just 1, min = Just 0.9, stdev = Just 0.0066666666666666706 }
        ,{ max = Just 2.2, mean = Just 2, min = Just 1.8, stdev = Just 0.04000000000000002 }
 -}
-get : Data -> List MaybeErrorStats
+get : Data -> List ErrorStats
 get data =
     data
         |> getErrorData2
@@ -48,7 +117,7 @@ rawMean xs =
                 Just <| (List.sum xs) / n
 
 
-errorStatsFromErrorDatum : ErrorDatum -> MaybeErrorStats
+errorStatsFromErrorDatum : ErrorDatum -> ErrorStats
 errorStatsFromErrorDatum d =
     let
         n_ =
@@ -60,19 +129,12 @@ errorStatsFromErrorDatum d =
         m =
             rawMean d.ys
     in
-        { mean = m
+        { x = d.x
+        , mean = m
         , min = List.minimum d.ys
         , max = List.maximum d.ys
         , stdev = rawStdev m d.ys
         }
-
-
-type alias ErrorData =
-    List ErrorDatum
-
-
-type alias ErrorDictionary =
-    Dict Float (List Float)
 
 
 emptyErrorDictionary : ErrorDictionary
@@ -124,14 +186,3 @@ rawStdev rawMean_ xs =
 
             ( _, _ ) ->
                 Nothing
-
-
-
--- |> List.map
--- groupByX : Data -> ErrorData
--- groupByX data =
---     let
---         xs =
---             List.sortBy (\p -> p.x)
---     in
---         xs
