cf462c757df5ef19a01df0e4b05f899fd215f4e4
James Carlson <jxxcarlson@mac.com>
2019-02-22 10:18:52 -0500

    Add row and column functions in table

--- src/Table.elm
+++ src/Table.elm
@@ -11,6 +11,33 @@ type alias Table a =
     List (Column a)
 
 
+testTable =
+    [ col1, col2 ]
+
+
+col1 =
+    [ 1, 2, 3 ]
+
+
+col2 =
+    [ 10, 20, 30 ]
+
+
+row : Int -> Table a -> Maybe (List a)
+row k table =
+    List.map (Utility.listGetAt k) table |> Utility.maybeCombine
+
+
+nRows : Table a -> Int
+nRows table =
+    List.length table
+
+
+nCols : Table a -> Int
+nCols table =
+    Maybe.map List.length (List.head table) |> Maybe.withDefault 0
+
+
 {-| getColumn i table extracts a list of Floats from
 a table (List of Records) by extracting column i of the
 Table, transforming the result to lists of floats.
